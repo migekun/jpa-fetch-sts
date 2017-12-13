05fd8276c895b9cccfa04f886b1378398be673a4
Miguel Angel Navas <manavas@gmail.com>
2017-12-13 08:59:22 +0100

    Fetch policy

---
+++ src/main/java/org/formacio/repositori/PersonaRepository.java
@@ -0,0 +1,12 @@
+package org.formacio.repositori;
+
+import org.formacio.domain.Persona;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface PersonaRepository extends CrudRepository<Persona, Long> {
+
+	@Query(value="select persona from Persona persona join fetch persona.cotxes where persona.id = :id")
+	Persona carrega (@Param("id") long id);
+}
